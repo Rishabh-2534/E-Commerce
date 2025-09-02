6ec24fc61e9946c14e1f9263756adbb8312c997b
Rishabh <rishabh.mishra@simpplr.com>
2025-09-02 13:40:27 +0530

    added Schema

---
+++ Schema.js
@@ -0,0 +1,11 @@
+/*
+User(User_id,UserName,Password,Role)
+Seller(User_id,seller_id,Name,verified,AddressId,account_id)
+Buyer(User_id,Name,contact,AddressId)
+Admin(User_id,Name)
+Product(Product_id,seller_id,category,Price,Brand,verified,Description)
+Order(Order_id,Product_id,Quantity,Price,Paid,Received)
+Cart(cart_id,User_id)
+Cart_items(cart_id,productId,Quanity);
+*/
+
