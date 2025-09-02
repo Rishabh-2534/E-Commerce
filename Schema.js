e6008b2b30e1c913c9f1c847f50a6a47e14f5353
Rishabh <rishabh.mishra@simpplr.com>
2025-09-02 14:00:31 +0530

    added schema structure

--- Schema.js
+++ Schema.js
@@ -8,4 +8,57 @@ Order(Order_id,Product_id,Quantity,Price,Paid,Received)
 Cart(cart_id,User_id)
 Cart_items(cart_id,productId,Quanity);
 */
+const UserSchema= {
+    userId:String,
+    userName:String,
+    password:String,
+    role:String,
+}
 
+const SellerSchema= {
+    userId: ObjectId,
+    sellerId: String,
+    name:String,
+    verified:Boolean,
+    addressId:String,
+    accountId:String,
+}
+
+const buyerSchema= {
+    userId:ObjectId,
+    name:String,
+    contact:String,
+    addressId:String,
+}
+
+const adminSchema= {
+    userId:ObjectId,
+    name:String,
+}
+
+const ProductSchema= {
+    sellerId: ObjectId,
+    category: String,
+    price:Number,
+    brand:String,
+    verified:Boolean,
+    description:String,
+}
+
+const OrderSchema ={
+    productId:ObjectId,
+    quantity:Number,
+    price:Number,
+    paid:Boolean,
+    received:Boolean,
+}
+
+const cartSchema={
+    UserId:ObjectId,
+}
+
+const cartItemSchema={
+    cartId:ObjectId,
+    productId:ObjectId,
+    quantity:Number,
+}
